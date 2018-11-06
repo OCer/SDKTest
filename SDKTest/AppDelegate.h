6209e3bfbdc3b198c6b601babb15f399d6c88009
Cer <cer@ztj.com>
2018-11-06 17:43:27 +0800

    Initial Commit

---
+++ SDKTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDKTest
+//
+//  Created by Cer on 2018/11/6.
+//  Copyright © 2018 Cer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
